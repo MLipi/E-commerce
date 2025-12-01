2a10a8a4fa14c15f7593b07bdf65db6484098524
MLipi <lipi1lopa@gmail.com>
2025-12-01 20:32:24 +0530

    msg

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,13 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import React from "react";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});
+}) ;
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
